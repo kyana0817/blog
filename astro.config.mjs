b6a0903a63b22f630ae47b9e464307d636189bda
kazeyanagi <kyana0817@gmail.com>
2024-09-19 22:56:02 +0900

    :sparkles: add mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import expressiveCode from 'astro-expressive-code'
+import remarkMermaid from 'remark-mermaidjs';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMermaid]
+	},
+	integrations: [expressiveCode(), mdx(), sitemap()],
 });
