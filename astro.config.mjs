4d2ecff287f7f1d99612d002a4ddb4c0f9d1adeb
kazeyanagi <kyana0817@gmail.com>
2024-09-22 17:25:44 +0900

    :sparkles: Add formatter with biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import expressiveCode from 'astro-expressive-code'
-import remarkMermaid from 'remark-mermaidjs';
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import expressiveCode from "astro-expressive-code"
+import remarkMermaid from "remark-mermaidjs"
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.kyana.dev',
-	markdown: {
-		remarkPlugins: [remarkMermaid]
-	},
-	integrations: [expressiveCode(), mdx(), sitemap()],
-});
+  site: "https://blog.kyana.dev",
+  markdown: {
+    remarkPlugins: [remarkMermaid],
+  },
+  integrations: [expressiveCode(), mdx(), sitemap()],
+})
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,24 @@
-import { defineCollection, z } from 'astro:content';
-
+import { defineCollection, z } from "astro:content"
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-
-const scrap = defineCollection({
-	type: 'content',
-	schema: z.object({
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 })
 
-export const collections = { blog, scrap };
+const scrap = defineCollection({
+  type: "content",
+  schema: z.object({
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+})
+
+export const collections = { blog, scrap }
